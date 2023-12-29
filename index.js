eda8a15f1ca11f1805990707cf8ea1b7162ad7a5
mikemills254 <mikemills930@gmail.com>
2023-12-29 15:30:51 +0300

    this update hope it helps me upload the api easily

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import Database  from "./Utils/Database.js";
-import router from './Routes/Routes.js';
-import cors from 'cors';
-
-dotenv.config();
-
-const app = express();
+import express from 'express'
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
-app.use(express.json());
+const app = express()
 
 app.get('/', (req, res) => {
     return res.send({ message: "Welcome to Taskfy" });
 });
 
-app.use('/api', router);
-
-(async () => {
-    try {
-        const client = await Database.connect();
-        if (client) {
-            console.log("Database connected successfully");
-            app.listen(PORT, () => {
-                console.log(`Server is running on port ${PORT}`);
-            });
-        }
-    } catch (error) {
-        console.log('Error connecting to the database: ' + error.message);
-    }
-})();
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
